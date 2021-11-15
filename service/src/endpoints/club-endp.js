80065cf5f44f84a6fe81a6bfefd8139f4e7dd11d
Annar Gaustad <annar.gaustad@gmail.com>
2021-11-15 21:58:52 +0100

    more club endpoints

--- service/src/endpoints/club-endp.js
+++ service/src/endpoints/club-endp.js
@@ -16,11 +16,23 @@ class ClubEndpoints {
         console.log(`${Helpers.getDateNowString()} Starting enpoints for club`);
         const MODULE = "club";
         const ALL_CLUBS = "getallclubs";
+        const ONE_CLUB = "getclubbyid";
+        const CLUB_REVISIONS = "getclubrevisionsbyid";
 
         this.app.get(`/${this.rootPath}/${MODULE}/${ALL_CLUBS}`, (req, res, next) => {
             this.repository.getAllClubs(res);
             console.log(`${Helpers.getDateNowString()} request: GET ${ALL_CLUBS}. req:${JSON.stringify(req.params)}`);
-        })
+        });
+
+        this.app.get(`/${this.rootPath}/${MODULE}/${ONE_CLUB}/:id`, (req, res, next) => {
+            this.repository.getClubById(res, req.params.id);
+            console.log(`${Helpers.getDateNowString()} request: GET ${ONE_CLUB}. req:${JSON.stringify(req.params)}`);
+        });
+
+        this.app.get(`/${this.rootPath}/${MODULE}/${CLUB_REVISIONS}/:id`, (req, res, next) => {
+            this.repository.getClubRevisionsById(res, req.params.id);
+            console.log(`${Helpers.getDateNowString()} request: GET ${CLUB_REVISIONS}. req:${JSON.stringify(req.params)}`);
+        });
     }
 }
 
--- service/src/repositories/club_repository.js
+++ service/src/repositories/club_repository.js
@@ -7,12 +7,12 @@ class ClubRepository {
     }
 
     // Get all clubs
-    getAllClubs(res){
+    getAllClubs(res) {
         var sql = "SELECT Club.Id, Cr.Name, Cr.Updated FROM Club JOIN (SELECT * FROM ClubRevision cr1 WHERE cr1.Updated = (SELECT MAX(Updated) FROM ClubRevision cr2 WHERE cr2.Id = cr1.Id)) cr ON club.Id = cr.ClubId";
         var params = [];
         this.db.all(sql, params, (err, rows) => {
             if (err) {
-                console.error(`${this.getdns()} ERROR: ${err.message}`);
+                console.error(`${Helpers.getDateNowString()} ERROR: ${err.message}`);
                 res.status(400).json({ "error": err.message });
                 return;
             }
@@ -25,8 +25,41 @@ class ClubRepository {
     }
 
     // Get Club by Id
-    getClubById(res){
-        
+    getClubById(res, id) {
+        var sql = "SELECT Club.Id, Cr.Name, Cr.Updated FROM Club JOIN (SELECT * FROM ClubRevision cr1 WHERE cr1.Updated = (SELECT MAX(Updated) FROM ClubRevision cr2 WHERE cr2.Id = cr1.Id)) cr ON club.Id = cr.ClubId WHERE Club.Id = ?"
+        var params = [id];
+        this.db.get(sql, params, (err, row) => {
+            if (err) {
+                console.error(`${Helpers.getDateNowString()} ERROR: ${err.message}`);
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+            res.json({
+                "message": "success",
+                "data": row
+            });
+            console.log(`${Helpers.getDateNowString()} getClubById returns Id ${row.Id}`);
+        });
+    }
+
+    // Get Club revisions
+    getClubRevisionsById(res, id) {
+        var sql = "SELECT Id, ClubId, Name, Updated FROM ClubRevision WHERE ClubId = ? ORDER BY Updated";
+        var params = [id];
+        this.db.all(sql, params, (err, rows) => {
+            if (err) {
+                console.error(`${Helpers.getDateNowString()} ERROR: ${err.message}`);
+                res.status(400).json({ "error": err.message });
+                return;
+            }
+
+            res.json({
+                "message": "success",
+                "data": rows
+            });
+            console.log(`${Helpers.getDateNowString()} getClubRevisionsById returns ${rows.length} rows for id ${id}`);
+        });
+
     }
 }
 
