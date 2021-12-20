7b7daeb83245ab16eb6219f03d27694001a7e3e0
Annar Gaustad <annar.gaustad@gmail.com>
2021-12-20 22:28:48 +0100

    missing module

--- hall-of-fame-app/src/app/app.module.ts
+++ hall-of-fame-app/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -32,7 +32,8 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     AppRoutingModule,
     CoreModule,
     PipesModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
