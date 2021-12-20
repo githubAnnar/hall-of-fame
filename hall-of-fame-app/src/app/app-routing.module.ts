60a95fd28231dbb67a4e7ffae8c706328b2253d4
Annar Gaustad <annar.gaustad@gmail.com>
2021-12-20 22:35:06 +0100

    routing

--- hall-of-fame-app/src/app/app-routing.module.ts
+++ hall-of-fame-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '', pathMatch: 'full', redirectTo: '/clubs' },
   { path: '**', pathMatch: 'full', redirectTo: '/clubs' }
 ];
